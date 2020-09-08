cefb19b29f1c120f51e6678800b0972bc303ae17
Mikael <nilssonmikael90@gmail.com>
2020-09-08 20:32:45 +0200

    Adding validating + tests to ProfileEdit

--- client/src/views/Profile/ProfileEdit.vue
+++ client/src/views/Profile/ProfileEdit.vue
@@ -6,123 +6,142 @@
           Please let us know if you have any questions or suggestions on how to improve Learn Every
           Day!
         </div>
-        <div class="flex flex-wrap -mx-3 mb-6">
-          <div class="w-full px-3">
-            <label
-              for="usernameInput"
-              class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-            >Email</label>
-            <input
-              id="usernameInput"
-              type="email"
-              placeholder="Email"
-              v-model="user.username"
-              class="appearance-none block w-full bg-gray-200 text-gray-700 mb-3 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-            />
+        <form id="ProfileEditForm">
+          <div class="flex flex-wrap -mx-3 mb-6">
+            <div class="w-full px-3">
+              <label
+                for="usernameInput"
+                class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+              >Username</label>
+              <input
+                id="usernameInput"
+                type="email"
+                placeholder="Username"
+                v-model="user.username"
+                :class="{ 'border-red-500 mb-3': $v.user.username.$error }"
+                class="appearance-none block w-full bg-gray-200 text-gray-700 mb-3 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+              />
+              <p
+                v-if="$v.user.username.$error"
+                class="text-red-500 text-xs italic"
+              >Enter your username.</p>
+            </div>
           </div>
-        </div>
-        <div class="flex flex-wrap -mx-3 mb-6">
-          <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
-            <label
-              for="firstNameInput"
-              class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-            >First name</label>
-            <input
-              id="firstNameInput"
-              type="text"
-              placeholder="First name"
-              v-model="user.first_name"
-              class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-            />
+          <div class="flex flex-wrap -mx-3 mb-6">
+            <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+              <label
+                for="firstNameInput"
+                class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+              >First name</label>
+              <input
+                id="firstNameInput"
+                type="text"
+                placeholder="First name"
+                v-model="user.first_name"
+                :class="{ 'border-red-500 mb-3': $v.user.first_name.$error }"
+                class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+              />
+              <p
+                v-if="$v.user.first_name.$error"
+                class="text-red-500 text-xs italic"
+              >Enter your first name.</p>
+            </div>
+            <div class="w-full md:w-1/2 px-3">
+              <label
+                for="lastNameInput"
+                class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+              >Last name</label>
+              <input
+                id="lastNameInput"
+                type="text"
+                placeholder="Last name"
+                v-model="user.last_name"
+                :class="{ 'border-red-500 mb-3': $v.user.last_name.$error }"
+                class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+              />
+              <p
+                v-if="$v.user.last_name.$error"
+                class="text-red-500 text-xs italic"
+              >Enter your last name.</p>
+            </div>
           </div>
-          <div class="w-full md:w-1/2 px-3">
-            <label
-              for="lastNameInput"
-              class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-            >Last name</label>
-            <input
-              id="lastNameInput"
-              type="text"
-              placeholder="Last name"
-              v-model="user.last_name"
-              class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-            />
+          <div class="flex flex-wrap -mx-3 mb-6">
+            <div class="w-full px-3">
+              <label
+                for="emailInput"
+                class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+              >Email</label>
+              <input
+                id="emailInput"
+                type="email"
+                placeholder="Email"
+                v-model="user.email"
+                :class="{ 'border-red-500 mb-3': $v.user.email.$error }"
+                class="appearance-none block w-full bg-gray-200 text-gray-700 mb-3 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+              />
+              <p v-if="$v.user.email.$error" class="text-red-500 text-xs italic">Enter your email.</p>
+            </div>
           </div>
-        </div>
-        <div class="flex flex-wrap -mx-3 mb-6">
-          <div class="w-full px-3">
-            <label
-              for="emailInput"
-              class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-            >Email</label>
-            <input
-              id="emailInput"
-              type="email"
-              placeholder="Email"
-              v-model="user.email"
-              class="appearance-none block w-full bg-gray-200 text-gray-700 mb-3 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-            />
+          <div class="flex flex-wrap -mx-3 mb-6">
+            <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+              <label
+                for="cityInput"
+                class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+              >City</label>
+              <input
+                id="cityInput"
+                type="text"
+                placeholder="city"
+                v-model="user.city"
+                class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+              />
+            </div>
+            <div class="w-full md:w-1/2 px-3">
+              <label
+                for="addressInput"
+                class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+              >Address</label>
+              <input
+                id="addressInput"
+                type="text"
+                placeholder="Address"
+                v-model="user.address"
+                class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+              />
+            </div>
           </div>
-        </div>
-        <div class="flex flex-wrap -mx-3 mb-6">
-          <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
-            <label
-              for="cityInput"
-              class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-            >City</label>
-            <input
-              id="cityInput"
-              type="text"
-              placeholder="city"
-              v-model="user.city"
-              class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-            />
+          <div class="flex flex-wrap -mx-3 mb-6">
+            <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+              <label
+                for="zipCodeInput"
+                class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+              >Zip code</label>
+              <input
+                id="zipCodeInput"
+                type="text"
+                placeholder="Zip code"
+                v-model="user.zip_code"
+                class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+              />
+            </div>
+            <div class="w-full md:w-1/2 px-3">
+              <label
+                for="phoneInput"
+                class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+              >Phone number</label>
+              <input
+                id="phoneInput"
+                type="text"
+                placeholder="Phone number"
+                v-model="user.phone"
+                class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+              />
+            </div>
+            <div class="flex items-center justify-between">
+              <button type="submit" class="btn btn-blue" @click.prevent="onSubmit">Save</button>
+            </div>
           </div>
-          <div class="w-full md:w-1/2 px-3">
-            <label
-              for="addressInput"
-              class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-            >Address</label>
-            <input
-              id="addressInput"
-              type="text"
-              placeholder="Address"
-              v-model="user.address"
-              class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-            />
-          </div>
-        </div>
-        <div class="flex flex-wrap -mx-3 mb-6">
-          <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
-            <label
-              for="zipCodeInput"
-              class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-            >Zip code</label>
-            <input
-              id="zipCodeInput"
-              type="text"
-              placeholder="Zip code"
-              v-model="user.zip_code"
-              class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-            />
-          </div>
-          <div class="w-full md:w-1/2 px-3">
-            <label
-              for="phoneInput"
-              class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-            >Phone number</label>
-            <input
-              id="phoneInput"
-              type="text"
-              placeholder="Phone number"
-              v-model="user.phone"
-              class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-            />
-          </div>
-          <div class="flex flex-wrap justify-end">
-            <!-- <button class="btn btn-blue" @click="onSave">Save</button> -->
-          </div>
-        </div>
+        </form>
       </div>
     </div>
   </div>
@@ -130,10 +149,42 @@
 
 <script>
 import { mapState } from "vuex";
+import { required, email } from "vuelidate/lib/validators";
 
 export default {
   computed: {
     ...mapState("auth", ["user"])
+  },
+  methods: {
+    async onSubmit() {
+      this.$v.$touch();
+
+      if (
+        this.$v.user.username.$error ||
+        this.$v.user.first_name.$error ||
+        this.$v.user.last_name.$error ||
+        this.$v.user.email.$error
+      )
+        return;
+    }
+  },
+  validations: {
+    user: {
+      username: {
+        required,
+        email
+      },
+      first_name: {
+        required
+      },
+      last_name: {
+        required
+      },
+      email: {
+        required,
+        email
+      }
+    }
   }
 };
 </script>
--- client/tests/unit/Views/profile/profile-edit.spec.js
+++ client/tests/unit/Views/profile/profile-edit.spec.js
@@ -1,30 +1,31 @@
 import { shallowMount, createLocalVue } from "@vue/test-utils";
 import Vuex from "vuex";
+import Vuelidate from "vuelidate";
 
 import ProfileEdit from "@/views/Profile/ProfileEdit";
 
 const localVue = createLocalVue();
-
 localVue.use(Vuex);
+localVue.use(Vuelidate);
 
 describe("ProfileEdit.vue", () => {
   let state;
   let store;
 
-  const defaultUser = {
-    username: "joedoe@mail.com",
-    first_name: "Joe",
-    last_name: "Doe",
-    email: "joedoe@mail.com",
-    address: "",
-    zip_code: "",
-    city: "",
-    phone: "",
-    disabled: false,
-    banned: false
-  };
-
   beforeEach(() => {
+    const defaultUser = {
+      username: "joedoe@mail.com",
+      first_name: "Joe",
+      last_name: "Doe",
+      email: "joedoe@mail.com",
+      address: "",
+      zip_code: "",
+      city: "",
+      phone: "",
+      disabled: false,
+      banned: false
+    };
+
     state = {
       user: defaultUser
     };
@@ -45,10 +46,42 @@ describe("ProfileEdit.vue", () => {
     expect(wrapper.find("#usernameInput").element.value).toBe(state.user.username);
   });
 
+  it("the username field is required", async () => {
+    const wrapper = shallowMount(ProfileEdit, { store, localVue });
+    wrapper.vm.$v.$touch();
+
+    const usernameInput = wrapper.find("#usernameInput");
+    const form = wrapper.find("#ProfileEditForm");
+
+    usernameInput.element.value = "";
+    usernameInput.trigger("input");
+    form.trigger("submit.prevent");
+
+    await wrapper.vm.$nextTick();
+
+    expect(wrapper.vm.$v.user.username.$error).toBe(true);
+  });
+
   it("has a first name field", () => {
     const wrapper = shallowMount(ProfileEdit, { store, localVue });
 
     expect(wrapper.find("#firstNameInput").element.value).toBe(state.user.first_name);
+  });
+
+  it("the first name field is required", async () => {
+    const wrapper = shallowMount(ProfileEdit, { store, localVue });
+    wrapper.vm.$v.$touch();
+
+    const firstNameInput = wrapper.find("#firstNameInput");
+    const form = wrapper.find("#ProfileEditForm");
+
+    firstNameInput.element.value = "";
+    firstNameInput.trigger("input");
+    form.trigger("submit.prevent");
+
+    await wrapper.vm.$nextTick();
+
+    expect(wrapper.vm.$v.user.first_name.$error).toBe(true);
   });
 
   it("has a last name field", () => {
@@ -57,10 +90,42 @@ describe("ProfileEdit.vue", () => {
     expect(wrapper.find("#lastNameInput").element.value).toBe(state.user.last_name);
   });
 
+  it("the last name field is required", async () => {
+    const wrapper = shallowMount(ProfileEdit, { store, localVue });
+    wrapper.vm.$v.$touch();
+
+    const lastNameInput = wrapper.find("#lastNameInput");
+    const form = wrapper.find("#ProfileEditForm");
+
+    lastNameInput.element.value = "";
+    lastNameInput.trigger("input");
+    form.trigger("submit.prevent");
+
+    await wrapper.vm.$nextTick();
+
+    expect(wrapper.vm.$v.user.last_name.$error).toBe(true);
+  });
+
   it("has a email field", () => {
     const wrapper = shallowMount(ProfileEdit, { store, localVue });
 
     expect(wrapper.find("#emailInput").element.value).toBe(state.user.email);
+  });
+
+  it("the email field is required", async () => {
+    const wrapper = shallowMount(ProfileEdit, { store, localVue });
+    wrapper.vm.$v.$touch();
+
+    const emailInput = wrapper.find("#emailInput");
+    const form = wrapper.find("#ProfileEditForm");
+
+    emailInput.element.value = "";
+    emailInput.trigger("input");
+    form.trigger("submit.prevent");
+
+    await wrapper.vm.$nextTick();
+
+    expect(wrapper.vm.$v.user.email.$error).toBe(true);
   });
 
   it("has a city field", () => {
@@ -69,10 +134,42 @@ describe("ProfileEdit.vue", () => {
     expect(wrapper.find("#cityInput").element.value).toBe(state.user.city);
   });
 
+  it("the city field is not required", async () => {
+    const wrapper = shallowMount(ProfileEdit, { store, localVue });
+    wrapper.vm.$v.$touch();
+
+    const cityInput = wrapper.find("#cityInput");
+    const form = wrapper.find("#ProfileEditForm");
+
+    cityInput.element.value = "";
+    cityInput.trigger("input");
+    form.trigger("submit.prevent");
+
+    await wrapper.vm.$nextTick();
+
+    expect(wrapper.vm.$v.user.$error).toBe(false);
+  });
+
   it("has a address field", () => {
     const wrapper = shallowMount(ProfileEdit, { store, localVue });
 
     expect(wrapper.find("#addressInput").element.value).toBe(state.user.address);
+  });
+
+  it("the address field is not required", async () => {
+    const wrapper = shallowMount(ProfileEdit, { store, localVue });
+    wrapper.vm.$v.$touch();
+
+    const addressInput = wrapper.find("#addressInput");
+    const form = wrapper.find("#ProfileEditForm");
+
+    addressInput.element.value = "";
+    addressInput.trigger("input");
+    form.trigger("submit.prevent");
+
+    await wrapper.vm.$nextTick();
+
+    expect(wrapper.vm.$v.user.$error).toBe(false);
   });
 
   it("has a zip code field", () => {
@@ -81,9 +178,41 @@ describe("ProfileEdit.vue", () => {
     expect(wrapper.find("#zipCodeInput").element.value).toBe(state.user.zip_code);
   });
 
+  it("the zip code field is not required", async () => {
+    const wrapper = shallowMount(ProfileEdit, { store, localVue });
+    wrapper.vm.$v.$touch();
+
+    const zipCodeInput = wrapper.find("#zipCodeInput");
+    const form = wrapper.find("#ProfileEditForm");
+
+    zipCodeInput.element.value = "";
+    zipCodeInput.trigger("input");
+    form.trigger("submit.prevent");
+
+    await wrapper.vm.$nextTick();
+
+    expect(wrapper.vm.$v.user.$error).toBe(false);
+  });
+
   it("has a phone field", () => {
     const wrapper = shallowMount(ProfileEdit, { store, localVue });
 
     expect(wrapper.find("#phoneInput").element.value).toBe(state.user.phone);
+  });
+
+  it("the phone field is not required", async () => {
+    const wrapper = shallowMount(ProfileEdit, { store, localVue });
+    wrapper.vm.$v.$touch();
+
+    const phoneInput = wrapper.find("#phoneInput");
+    const form = wrapper.find("#ProfileEditForm");
+
+    phoneInput.element.value = "";
+    phoneInput.trigger("input");
+    form.trigger("submit.prevent");
+
+    await wrapper.vm.$nextTick();
+
+    expect(wrapper.vm.$v.user.$error).toBe(false);
   });
 });
