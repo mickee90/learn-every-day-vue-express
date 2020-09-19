58a3f4d3409fc79b3297099ccef9d29a87859e0d
Mikael <nilssonmikael90@gmail.com>
2020-09-19 15:28:45 +0200

    Refactor Posts tests

--- client/src/views/Posts/Posts.spec.js
+++ client/src/views/Posts/Posts.spec.js
@@ -5,9 +5,16 @@ import { shallowMount } from "@vue/test-utils";
 describe("@/views/Posts/Posts.vue", () => {
   let wrapper;
   let authState;
-  let postState;
+  let state;
+  let posts;
 
   beforeEach(() => {
+    posts = [
+      { id: 1, user_id: 1, title: "First post" },
+      { id: 2, user_id: 1, title: "Second post" },
+      { id: 3, user_id: 1, title: "Third post" }
+    ];
+
     authState = {
       user: {
         id: 1,
@@ -15,46 +22,38 @@ describe("@/views/Posts/Posts.vue", () => {
       },
       isLoggedIn: true
     };
+    state = { posts: null };
+
+    wrapper = mountWithStore(authState, state);
   });
+
   it("Lists correct number of posts", async () => {
-    postState = {
-      posts: [
-        { id: 1, user_id: 1, title: "First post" },
-        { id: 2, user_id: 1, title: "Second post" },
-        { id: 3, user_id: 1, title: "Third post" }
-      ]
-    };
-    wrapper = mountWithStore(authState, postState);
+    state.posts = posts;
+    await wrapper.vm.$nextTick();
+
     expect(wrapper.findAll(".post-item").length).toBe(3);
   });
-  // @TODO
+  // @TODO: Requires axios + backend api mock
   // it("Only lists the posts made by the user", async () => {});
 
   // @TODO: Create a global loader
   // @TODO: Check if the loader disappears after the fetch is done
   it("It shows a loader before the posts is fetched", async () => {
-    postState = { posts: null };
-    wrapper = mountWithStore(authState, postState);
-
     expect(wrapper.text()).toContain("Loader");
   });
   it("Shows text to explain if the user does not have any posts", async () => {
-    postState = { posts: [] };
-    wrapper = mountWithStore(authState, postState);
+    state.posts = [];
+    await wrapper.vm.$nextTick();
 
     expect(wrapper.text()).toContain("You have not created any posts yet.");
   });
   it("Includes a 'create new post'-button", async () => {
-    wrapper = mountWithStore(authState, postState);
-
-    const createPostBtn = wrapper.find("#create_post_btn");
-
-    expect(createPostBtn.text()).toContain("New");
+    expect(wrapper.find("#create_post_btn").text()).toContain("New");
   });
 });
 
-function mountWithStore(authState, postState) {
-  const postActions = {
+function mountWithStore(authState, state) {
+  const actions = {
     init: jest.fn()
   };
 
@@ -67,8 +66,8 @@ function mountWithStore(authState, postState) {
         },
         posts: {
           namespaced: true,
-          state: { ...postState },
-          actions: { ...postActions }
+          state,
+          actions
         }
       },
       stubs: {
